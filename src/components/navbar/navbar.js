7985ab013a6bded366aa0af208942cf2e8526549
Tomás <tomas.pacheco.c@gmail.com>
2023-06-23 16:15:17 -0400

    initial setup

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './components/navbar/navbar.sass'
+
+export class navbar extends Component {
+  render() {
+    return (
+      <div>navbar component</div>
+    )
+  }
+}
+
+export default navbar
