37cbe2e0e8b1693810c92d0b8e87b9cf39f73827
tomas.pacheco <tomas.p@cheil.com>
2023-06-28 15:18:22 -0400

    footer year function fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react'
 
 export class Footer extends Component {
   render() {
+    const pageName = 'Blog Page';
+    const currentYear = new Date().getFullYear();
+    let mainFooterInfo = pageName + ' | ' + currentYear;
     return (
       <div className='footer-container'>
-        <p>Al Margen | 2023</p>
+        <span>{mainFooterInfo}</span>
       </div>
     )
   }
